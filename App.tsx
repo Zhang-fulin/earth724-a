a6163ebb36c9274e875a183fe6577389c4aaf2d4
zhangfulin <1148112937@qq.com>
2026-01-04 15:07:06 +0800

    [change to https]

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ export default function App() {
       <StatusBar style="light" />
       <SafeAreaView style={{ flex: 1 }}>
         <WebView
-          source={{ uri: 'http://www.webcad.online/index.html' }}
+          source={{ uri: 'https://www.webcad.online/index.html' }}
           style={styles.webview}
           onLoadStart={() => { setLoading(true); console.log('start'); }}
           onLoadEnd={() => { console.log('end'); setTimeout(() => setLoading(false), 3000); }}
